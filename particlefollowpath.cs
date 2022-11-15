3614c8faf767a40f4f38be2c6d72c9c4438a2f18
binghen1994 <72238711+binghen1994@users.noreply.github.com>
2022-11-14 20:05:31 -0800

    Add files via upload

---
+++ particlefollowpath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particlefollowpath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string pathName;
+    public float time;
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeInOutSine, "time", time));
+    }
+
+}
